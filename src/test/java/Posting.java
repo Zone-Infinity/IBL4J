88dc16eb9ec3ff43777494aa1b68f2d271f28f70
Zone-Infinity <sohamdokhale1602@gmail.com>
2021-05-06 18:31:43 +0530

    Add Tests

---
+++ src/test/java/Posting.java
@@ -0,0 +1,9 @@
+import me.infinity.ibl.IBL;
+
+public class Posting {
+    public static void main(String[] args) {
+        IBL ibl = new IBL.Builder(752052866809593906L, "");
+
+        ibl.postStats(109);
+    }
+}
